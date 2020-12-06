4d4e6e26ec9ba6bc32de1469cb08b282d3ab8eae
Robert Chaim <robak3731@gmail.com>
2020-12-06 17:38:23 +0100

    Add class CurrencyData

--- currency.py
+++ currency.py
@@ -2,12 +2,29 @@ import requests
 import csv
 from flask import Flask, render_template, request
 app = Flask(__name__)
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-currencies = []
+
+
+class CurrencyData:
+    def __init__(self):
+        self.currencies = []
+
+    def load_items_from_csv(self):
+        self.currencies.clear()
+        with open('currency.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
+            for row in reader:
+                self.currencies.append(row)
+
+    def current_data(self, currency):
+        self.load_items_from_csv()
+        for c in self.currencies:
+            if currency.upper() == c['code']:
+                return float(c['bid'])
 
 
 def get_current_data():
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
     with open('currency.csv', 'w', newline='') as file:
         fieldnames = ['currency', 'code', 'bid', 'ask']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
@@ -16,29 +33,20 @@ def get_current_data():
             writer.writerow(currency)
 
 
-def load_items_from_csv():
-    currencies.clear()
-    with open('currency.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',')
-        for row in reader:
-            currencies.append(row)
-
-
-def current_data(i):
-    load_items_from_csv()
-    for c in currencies:
-        if i.upper() == c['code']:
-            return float(c['bid'])
-
-
 @app.route('/currency', methods=['GET', 'POST'])
 def main():
     if request.method == 'GET':
         return render_template("submit.html")
     elif request.method == 'POST':
         get_current_data()
+        currency_data = CurrencyData()
         currency = request.form['currency']
         amount = float(request.form['amount'].replace(',', '.'))
-        new_amount = round(current_data(currency)*amount, 2)
+        currency_value = currency_data.current_data(currency)
+        new_amount = round(currency_value*amount, 2)
 
         return render_template("submit.html", currency=currency.upper(), amount=amount, new_amount=new_amount)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
